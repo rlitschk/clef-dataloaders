4fc034e0aa2479a5e3cec70c3b86198ca6f162ad
Robert Litschko <robert.litschko@lmu.de>
2024-10-26 19:06:31 +0200

    reformatting

--- clef_paths.py
+++ clef_paths.py
@@ -28,21 +28,30 @@ italian = {"2001": [it_lastampa, it_sda94],
 # Prepare finnish CLEF data paths
 aamu9495 = PATH_BASE_DOCUMENTS + "finnish/aamu/"
 fi_ammulethi9495 = (aamu9495, extract_finish_aamuleth9495)
-finnish = {"2001": None, "2002": [fi_ammulethi9495], "2003": [fi_ammulethi9495]}
+finnish = {
+  "2001": None,
+  "2002": [fi_ammulethi9495],
+  "2003": [fi_ammulethi9495]
+}
 
 # Prepare english CLEF data paths
 gh95 = (PATH_BASE_DOCUMENTS + "english/GH95/", extract_english_gh)
 latimes = (PATH_BASE_DOCUMENTS + "english/latimes/", extract_english_latimes)
-english = {"2001": [gh95, latimes],
-           "2002": [gh95, latimes],
-           "2003": [gh95, latimes]}
+english = {
+  "2001": [gh95, latimes],
+  "2002": [gh95, latimes],
+  "2003": [gh95, latimes]
+}
 
 # Prepare german CLEF data paths
 der_spiegel = (PATH_BASE_DOCUMENTS + "german/der_spiegel/", extract_german_derspiegel)
 fr_rundschau = (PATH_BASE_DOCUMENTS + "german/fr_rundschau/", extract_german_frrundschau)
 de_sda94 = (PATH_BASE_DOCUMENTS + "german/sda94/", extract_german_sda)
 de_sda95 = (PATH_BASE_DOCUMENTS + "german/sda95/", extract_german_sda)
-german = {"2003": [der_spiegel, fr_rundschau, de_sda94, de_sda95]}
+german = {
+  "2003": [der_spiegel, fr_rundschau, de_sda94, de_sda95],
+  "2002": [der_spiegel, fr_rundschau, de_sda94, de_sda95]
+}
 
 # Prepare russian CLEF data paths
 xml = (PATH_BASE_DOCUMENTS + "russian/xml/", extract_russian)
@@ -51,7 +60,7 @@ all_paths = {"nl": dutch, "it": italian, "fi": finnish, "en": english, "de": ger
 
 # Utility function
 languages = [("de", "german"), ("en", "english"), ("ru", "russian"), ("fi", "finnish"), ("it", "italian"),
-             ("fr", "french"), ("tr", "turkish")]
+             ("fr", "french"), ("tr", "turkish"), ("es", "spanish"), ("nl", "dutch"), ("sv", "sv")]
 short2pair = {elem[0]: elem for elem in languages}
 long2pair = {elem[1]: elem for elem in languages}
 def get_lang2pair(language):
